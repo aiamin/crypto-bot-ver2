945f1cf16d2e024ba1c195e2f6ccaf221fb71fe9
Amin <amin.mafn@gmail.com>
2022-04-26 02:43:38 +0430

    commit

--- main.py
+++ main.py
@@ -2,72 +2,95 @@ import pandas as pd
 import pandas_ta as ta
 import ccxt
 import requests
+import schedule
+import time
 from datetime import datetime
 
 
-discord_webhook_url = "https://discord.com/api/webhooks/962754927896195103/rC9nH7foyWOmQGpa9LRBOUF3s390UQe_1Lgs_S9I6FLmCVXvupY9_ZUXDHPuFvA5JxBI"
 exchange = ccxt.binance()
-symbol = 'BTC/USDT'
-interval = '5m'
+discord_webhook_url = "https://discord.com/api/webhooks/962754927896195103/rC9nH7foyWOmQGpa9LRBOUF3s390UQe_1Lgs_S9I6FLmCVXvupY9_ZUXDHPuFvA5JxBI"
+in_position = False
 
-def get_ohlcv(symbol, interval, limit=1000) -> pd.DataFrame:
 
-    # headers
-    columns = ['timestamp', 'open', 'high', 'low', 'close', 'volume']
+def add_ta(df):
+    
+    supertrend = df.ta.supertrend(period=7, multiplier=3)
+  
+    df_result = pd.concat([df,supertrend], axis=1)
+    
+    return df_result
 
-    # get candles from exchange
-    bars = exchange.fetch_ohlcv(symbol, timeframe=interval, limit=limit)
 
-    # convert list of list to pandas dataframe and delete last row
-    df = pd.DataFrame(bars[:-1], columns=columns)
+def check_buy_sell_signals(df):
+    global in_position
+    now_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    # convert millisecond to datetime
+    print("checking for buy and sell signals")
+    print(df.tail(4))
+    
+    last_row_index = len(df.index) - 1
+    previous_row_index = last_row_index - 1
+
+    
+    if df['SUPERTd_7_3.0'][previous_row_index] == -1 and df['SUPERTd_7_3.0'][last_row_index] == 1:
+        print("changed to uptrend, buy")
+        if not in_position:
+            # order = exchange.create_market_buy_order('BTC/USDT', 0.05)
+            # print(order)
+            message1 = f"---------------------------\nat: {now_time} \n**BUY at:** {df['close'][last_row_index]}\n---------------------------"
+            payload = {"username": "alertbot","content": message1}
+            requests.post(discord_webhook_url, json=payload)
+            in_position = True
+        else:
+            print("already in position, nothing to do")
+            message1 = f"---------------------------\nat: {now_time} \n**already in position**, nothing to do \n---------------------------"
+            payload = {"username": "alertbot","content": message1}
+            requests.post(discord_webhook_url, json=payload)
+
+            
+    if df['SUPERTd_7_3.0'][previous_row_index] == 1 and df['SUPERTd_7_3.0'][last_row_index] == -1:
+        if in_position:
+            print("changed to downtrend, sell")
+            # order = exchange.create_market_sell_order('BTC/USDT', 0.05)
+            # print(order)
+            message1 = f"---------------------------\nat: {now_time} \n**SELL at:** {df['close'][last_row_index]}\n---------------------------"
+            payload = {"username": "alertbot","content": message1}
+            requests.post(discord_webhook_url, json=payload)            
+            in_position = False
+        else:
+            print("You aren't in position, nothing to sell")
+            message1 = f"---------------------------\nat: {now_time} \n**You aren't in position**, nothing to sell \n---------------------------"
+            payload = {"username": "alertbot","content": message1}
+            requests.post(discord_webhook_url, json=payload)
+
+            
+    if df['SUPERTd_7_3.0'][previous_row_index] == df['SUPERTd_7_3.0'][last_row_index]:
+        print("Wait for change in trend")
+        message1 = f"---------------------------\nat: {now_time} \n**Wait for change in trend**, nothing to do\n---------------------------"
+        payload = {"username": "alertbot","content": message1}
+        requests.post(discord_webhook_url, json=payload)
+
+
+
+def run_bot():
+    
+    print('Fetching new bars for :')
+    print('Iran Time :' ,datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    print('UTC  Time :' ,datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"))   
+
+    
+    bars = exchange.fetch_ohlcv('BTC/USDT', timeframe='1m', limit=100)
+    df = pd.DataFrame(bars[:-1], columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
     df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
 
-    return df
-
-
-df_start = get_ohlcv(symbol, interval)
-
-
-supertrend = df_start.ta.supertrend(period=7, multiplier=3)
-macd = df_start.ta.macd(fast=14, slow=28)
-rsi = df_start.ta.rsi()
-adx = df_start.ta.adx()
-df_end = pd.concat([df_start,supertrend,macd,rsi,adx], axis=1)
-
-
-last_row = df_end.iloc[-1]
-
-
-if last_row['SUPERTd_7_3.0'] == 1:
-    if last_row['ADX_14'] < 25:
-        message1 = f"---------------------------\nat: {last_row['timestamp']} \nSupertrend Signal: **Buy** \nADX Signal: **NO TREND** \n**WAIT**\n---------------------------"
-        print(message1)
+    df_with_ta = add_ta(df)
     
-    if last_row['ADX_14'] >= 25:
-        if last_row['DMP_14'] > last_row['DMN_14']:
-            message1 = f"---------------------------\nat: {last_row['timestamp']} \nSupertrend Signal: **Buy** \nADX Signal: **STRONG UPTREND** \n**BUY at {last_row['close']}**\n---------------------------"
-            print(message1)    
-        if last_row['DMN_14'] > last_row['DMP_14']:
-            message1 = f"---------------------------\nat: {last_row['timestamp']} \nSupertrend Signal: **Buy** \nADX Signal: **STRONG DOWNTREND** \n**WTF**\n---------------------------"
-            print(message1)
-        
-
-    
-if last_row['SUPERTd_7_3.0'] == 0:
-    if last_row['ADX_14'] < 25:
-        message1 = f"---------------------------\nat: {last_row['timestamp']} \nSupertrend Signal: **SELL** \nADX Signal: **NO TREND** \n**WAIT**\n---------------------------"
-        print(message1)
-    
-    if last_row['ADX_14'] >= 25:
-        if last_row['DMP_14'] > last_row['DMN_14']:
-            message1 = f"---------------------------\nat: {last_row['timestamp']} \nSupertrend Signal: **SELL** \nADX Signal: **STRONG UPTREND** \n**WTF**\n---------------------------"
-            print(message1)    
-        if last_row['DMN_14'] > last_row['DMP_14']:
-            message1 = f"---------------------------\nat: {last_row['timestamp']} \nSupertrend Signal: **SELL** \nADX Signal: **STRONG DOWNTREND** \n**SELL at {last_row['close']}**\n---------------------------"
-            print(message1)
+    check_buy_sell_signals(df_with_ta)
 
 
-payload = {"username": "alertbot","content": message1 }
-requests.post(discord_webhook_url, json=payload)
+
+schedule.every(20).seconds.do(run_bot)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
